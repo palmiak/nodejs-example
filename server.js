c01ee1b8fef282ea514b1b386d04d90e99f6d904
Maciek Palmowski <m.palmowski@freshpixels.pl>
2022-12-09 10:48:41 +0100

    server change

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const http = require('node:http');
 
-const hostname = '127.0.0.1';
 const port = process.env.PORT ?? 3000;
 
 const server = http.createServer((req, res) => {
@@ -9,6 +8,6 @@ const server = http.createServer((req, res) => {
   res.end('Hello, Kinsta!\n');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, () => {
+  console.log(`Server running at port ${port}`);
 });
